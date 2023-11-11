bc2ec70492bc7cd7778db83be64414c2083445a0
cindy4KA <35273396+cindy4KA@users.noreply.github.com>
2023-11-11 14:37:13 -0600

    Update FrontExtend.java
    Comment added

--- src/main/java/org/usfirst/frc2704/comandrobot/commands/FrontExtend.java
+++ src/main/java/org/usfirst/frc2704/comandrobot/commands/FrontExtend.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+// Thank you Brent this is really good information. 
 package org.usfirst.frc2704.comandrobot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
